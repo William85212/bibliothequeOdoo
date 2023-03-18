46e33e12d3eaa2a08884ad9635dd8d0b363a31bd
William Hardy <william.hardy@medsoc.be>
2023-03-18 19:55:11 +0100

    feat: add etagères View and model

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from . import book
+from . import piece
 from . import bibliotheque
 from . import etageres
-from . import piece
--- models/etageres.py
+++ models/etageres.py
@@ -6,3 +6,10 @@ class bibliotheque(models.Model):
     _name = "bc_bib.etageres"
     _description="etagere"
 
+    etagereName = fields.Char(
+        string="title",
+        require="True",
+        help="coucou bhou",
+        index="True"
+    )
+
